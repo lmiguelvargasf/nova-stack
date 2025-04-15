25666d4459d4a9b1ad19c9e4011b28de8f01dcc9
M <lmiguelvargasf@gmail.com>
2025-04-15 18:14:31 -0500

    Set up piccolo ORM

--- backend/src/app/__init__.py
+++ backend/src/app/__init__.py
@@ -1,7 +1,28 @@
 from litestar import Litestar, get
+from piccolo.engine import engine_finder
+
+
+async def open_database_connection_pool():
+    try:
+        engine = engine_finder()
+        await engine.start_connection_pool()
+    except Exception:
+        print("Unable to connect to the database")
+
+
+async def close_database_connection_pool():
+    try:
+        engine = engine_finder()
+        await engine.close_connection_pool()
+    except Exception:
+        print("Unable to connect to the database")
 
 @get("/")
 async def get_root() -> str:
     return "Hello, World!"
 
-app = Litestar(route_handlers=[get_root])
+app = Litestar(
+    route_handlers=[get_root],
+    on_startup=[open_database_connection_pool],
+    on_shutdown=[close_database_connection_pool],
+)
