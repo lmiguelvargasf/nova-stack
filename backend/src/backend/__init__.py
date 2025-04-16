41e3ada5ad4a81f7af6de8ec3d6ac6dfec390944
M <lmiguelvargasf@gmail.com>
2025-04-15 21:43:25 -0500

    Add BaseUser to piccolo admin

--- backend/src/backend/__init__.py
+++ backend/src/backend/__init__.py
@@ -1,13 +1,13 @@
 from litestar import Litestar, get, asgi
 from litestar.types import Receive, Scope, Send
 from piccolo.engine import engine_finder
-from piccolo_admin.endpoints import create_admin
+from piccolo_admin.endpoints import create_admin, BaseUser
 
 
 
 @asgi("/admin/", is_mount=True, copy_scope=False)
 async def admin(scope: "Scope", receive: "Receive", send: "Send") -> None:
-    await create_admin(tables=[])(scope, receive, send)
+    await create_admin(tables=[BaseUser])(scope, receive, send)
 
 
 async def open_database_connection_pool():
