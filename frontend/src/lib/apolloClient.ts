7759df6bf1257609915a8dc3cca396f3461029fa
M <lmiguelvargasf@gmail.com>
2025-04-26 15:48:04 -0500

    Add Apollo Client setup for GraphQL integration in frontend

---
+++ frontend/src/lib/apolloClient.ts
@@ -0,0 +1,9 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri:
+    process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT || "http://localhost:8000/graphql",
+  cache: new InMemoryCache(),
+});
+
+export default client;
