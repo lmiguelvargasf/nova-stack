b10aebd2c632c41dad67df476ff4ae13c2af0a1c
M <lmiguelvargasf@gmail.com>
2025-04-26 14:03:00 -0500

    Add UserMutation for creating new users in GraphQL schema

--- backend/src/backend/apps/users/graphql/schema.py
+++ backend/src/backend/apps/users/graphql/schema.py
@@ -14,3 +14,26 @@ class UserQuery:
         if user is None:
             raise GraphQLError(f"User with id {user_id} not found")
         return UserType.from_model(user)
+
+
+@strawberry.type
+class UserMutation:
+    @strawberry.mutation
+    async def create_user(
+        self,
+        username: str,
+        first_name: str,
+        last_name: str,
+    ) -> UserType:
+        existing_user = await BaseUser.objects().get(BaseUser.username == username)
+        if existing_user:
+            raise GraphQLError(f"User with username '{username}' already exists.")
+
+        user = BaseUser(
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+        )
+        await user.save()
+
+        return UserType.from_model(user)
